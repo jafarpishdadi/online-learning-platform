4758d7ba72907ad204e50d2880a80ddd7465e2b5
kirusans <kirusans@hotmail.com>
2020-10-18 00:13:39 -0400

    added react

---
+++ doc/src/react/src/pages/login/login.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import Login from '../../components/createAccount/login.component'
+import './login.css'
+import OutsideNavbar from '../../components/navbar/outsideNavbar.js'
+import Picture from '../../assets/loginPicture.png'
+
+class LoginPage extends Component {
+	render() {
+		return(
+			<React.Fragment>
+				<OutsideNavbar/>
+				<div className="d-flex page">
+					<div className="picture">
+						<img src={Picture} alt="Map"></img>
+					</div>
+					<div className="login-component">
+						<Login/>
+					</div>
+				</div>
+			</React.Fragment>
+		)
+	}
+}
+export default LoginPage
