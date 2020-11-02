4c14e89162274c8f027e03fac9092f626634fc19
flyingzambie <26560603+flyingzambie@users.noreply.github.com>
2020-11-02 16:34:58 -0500

    Profile Page

--- src/react/src/pages/profile/profilePage.js
+++ src/react/src/pages/profile/profilePage.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
-import Sidebar from '../../components/sidebar/sidebar.component.js'
 import Profile from '../../components/profile/profile.component.js'
-import './profile.css'
+import './profilePage.css'
+import { Redirect } from 'react-router-dom';
+// Bunch of pictures
+import Headerbar from  '../..//components/headerbar/HeaderTaskbar.js'
+import Sidebar from '../..//components/sidebar/sidebar.component.js'
+import profile from '../../assets/profile.png'
+import classes from '../../assets/classes.png'
+import scheduling from '../../assets/scheduling.png'
+import community from '../../assets/community.png'
+import mailbox from '../../assets/mailbox.png'
+import achievements from '../../assets/achievements.png'
+import socialInitiatives from '../../assets/Welfare.png'
+import settings from '../../assets/settings.png'
+import dashboard from '../../assets/dashboard.png'
+// End of pictures
+
 
 let educations = [
     {id:1, text: "University of Toronto Scarbrough Undergraduate"},
@@ -37,24 +51,43 @@ let lastName = "Su"
 let email =  "Ling.Su@mail.utoronto.ca"
 let phoneNumber = "416-731-9960"
 
+let headerItems = {link: '/sidebar', title: 'Dashboard', profileImg: profile }
+
+let navItems = [
+    {id: 1, link: "", imgSrc: dashboard, title: "Dashboard" },
+    {id: 2, link: "", imgSrc: classes, title: "Classes" },
+    {id: 3, link: "", imgSrc: scheduling, title: "Scheduling" },
+    {id: 4, link: "", imgSrc: community, title: "Community" },
+    {id: 5, link: "", imgSrc: mailbox, title: "Mailbox" },
+    {id: 6, link: "", imgSrc: achievements, title: "Achievements" },
+    {id: 7, link: "", imgSrc: socialInitiatives, title: "Social Initiatives" },
+    {id: 8, link: "", imgSrc: settings, title: "Settings" }
+    ]
+
 class ProfilePage extends Component {
 	render() {
 		return(
-			<div>
-				<Profile 
-                educations = {educations} 
-                skills = {skills} 
-                completedCourses = {completedCourses} 
-                languages = {languages}
-                name = {name}
-                description = {description}
-                timeJoin = {timeJoin}
-                firstName = {firstName}
-                lastName = {lastName}
-                email =  {email}
-                phoneNumber = {phoneNumber}
-              />
-            </div>
+			<React.Fragment>
+                <Sidebar books={navItems}/>
+                <Headerbar icons={headerItems}/>
+                <div class= "box">
+                    <Profile 
+                        educations = {educations} 
+                        skills = {skills} 
+                        completedCourses = {completedCourses} 
+                        languages = {languages}
+                        name = {name}
+                        description = {description}
+                        timeJoin = {timeJoin}
+                        firstName = {firstName}
+                        lastName = {lastName}
+                        email =  {email}
+                        phoneNumber = {phoneNumber}
+                        />
+                </div>
+                    
+            </React.Fragment>
+			
 		)
 	}
 }
