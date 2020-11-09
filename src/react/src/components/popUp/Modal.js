b12e17403843d2d590f18ce01930733d79938a5b
Prashanth Ketheeswaran <prashanthketheeswaran@gmail.com>
2020-11-08 19:07:22 -0500

    Added some more

--- src/react/src/components/popUp/Modal.js
+++ src/react/src/components/popUp/Modal.js
@@ -3,21 +3,52 @@ import React from 'react'
 import axios from 'axios';
 
 let Modal;
-let value;
 
 
 export default Modal = ({ handleClose, show, children }) => {
 	const showHideClassName = show ? "modal display-block" : "modal display-none";
 
+	let state = {
+		time: '8 AM',
+		eventName:' ',
+		eventType: 'Workshop'
+	};
+	
+	function handleChangeTime(event) {
+		let value = event.target.value;
+		state.time = value;
+		console.log(state.time);
+	}
+
+	function handleChangeType(event) {
+        let value = event.target.value;
+		state.eventType = value;
+		console.log(state.eventType);
+	}
+
+	function handleChangeName(event) {
+        let value = event.target.value;
+		state.eventName = value;
+		console.log(state.eventName);
+	}
+
+	function onclick(event){
+		let name = event.target.name;
+		let value = event.target.value;
+		console.log(name,value)
+		let data ={};
+		data[name] = value;
+
+		this.setState(data)
+	}
+
 	 function submit(e) {
-        e.preventDefault();
-		console.log("Hello there");
-		console.log({value});
-        axios.post('http://127.0.0.1:8103/api/db_create_event', {email:this.state.user_type, start_time: this.state.email, event_type:this.state.username, name: this.state.password })
+		e.preventDefault();
+		let email = window.localStorage.getItem('email');
+        axios.post('http://127.0.0.1:8103/api/db_create_event', {'email': email, 'start_time': state.time, 'event_type': state.eventType, 'name': state.eventName, 'date': window.localStorage.getItem('selectedDay')})
             .then(response => {
-				console.log(response.data)
-				this.setState({accountCreated:true});
-				localStorage.setItem('token', response.data.auth.access_token)
+				console.log(response.data);
+				alert("Event joined successfully");
 			})
 			.catch((error) => {
 			console.log(error)
@@ -34,11 +65,11 @@ export default Modal = ({ handleClose, show, children }) => {
 				<form>
 					<div class="form-group pt-0 pl-2 pr-2">
 						<label for="inputEmail" class="text-dark font-weight-bold">Event Name</label>
-						<input type="text" class="form-control" name='eventName' aria-describedby="emailHelp" />
+						<input type="text" class="form-control" name='eventName' onChange={handleChangeName}/>
 					</div>
 					<div class="form-group pt-0 pl-2 pr-2">
 					<label class="text-dark font-weight-bold">Start Time</label>
-						<select class="form-control" id="exampleFormControlSelect1" value={value}>
+						<select class="form-control" name='time' onChange={handleChangeTime}>
 							<option>8 AM</option>
 							<option>9 AM</option>
 							<option>10 AM</option>
@@ -56,7 +87,7 @@ export default Modal = ({ handleClose, show, children }) => {
 					</div>
 					<div class="form-group pt-0 pl-2 pr-2">
 					<label class="text-dark font-weight-bold">Event Type</label>
-						<select class="form-control">
+						<select class="form-control" name='eventType' onChange={handleChangeType}>
 							<option>Workshop</option>
 							<option>Meeting</option>
 							<option>Study</option>
