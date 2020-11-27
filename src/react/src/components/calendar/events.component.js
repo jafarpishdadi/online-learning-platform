f5f024dff8834cf9791685fd1ba7526dc51feed5
Prashanth Ketheeswaran <prashanthketheeswaran@gmail.com>
2020-11-26 19:16:17 -0500

    Added event cards

--- src/react/src/components/calendar/events.component.js
+++ src/react/src/components/calendar/events.component.js
@@ -2,6 +2,53 @@ import React, { Component } from 'react'
 
 let vals;
 
+const EventItem = ({ name, eventType, eventInfo }) => {
+	if (eventType == "Workshop") {
+		return (
+			<div class="card bg-light m-2">
+				<div class="card-header">{eventType}</div>
+				<div class="card-body">
+					<h5>Primary card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				</div>
+			</div>
+		)
+	}
+	else if (eventType == "Meeting") {
+		return (
+			<div class="card text-white bg-warning m-2">
+				<div class="card-header">{eventType}</div>
+				<div class="card-body">
+					<h5>Primary card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				</div>
+			</div>
+		)
+	}
+	else if (eventType == "Study") {
+		return (
+			<div class="card text-white bg-primary m-2">
+				<div class="card-header">{eventType}</div>
+				<div class="card-body">
+					<h5>Primary card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				</div>
+			</div>
+		)
+	}
+	else if (eventType == "Class") {
+		return (
+			<div class="card text-white bg-info m-2">
+				<div class="card-header">{eventType}</div>
+				<div class="card-body">
+					<h5>Primary card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				</div>
+			</div>
+		)
+	}
+}
+
 class Events extends Component {
 
 	buildEvents() {
@@ -9,12 +56,20 @@ class Events extends Component {
 		if (vals) {
 			return (
 				vals.map((book) =>
-					<p>lol</p>
+					<EventItem
+						name={book.name}
+						eventType={book.event_type}
+						eventInfo={book.event_info}
+					/>
 				)
 			)
 		} else {
 			return (
-				<p>No events scheduled</p>
+				<div class="card border-light custom-no-events">
+					<div class="card-body">
+						No events scheduled
+  					</div>
+				</div>
 			)
 		}
 
@@ -23,7 +78,9 @@ class Events extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				{this.buildEvents()}
+				<div class="d-flex flex-column">
+					{this.buildEvents()}
+				</div>
 			</React.Fragment>
 		)
 	}
