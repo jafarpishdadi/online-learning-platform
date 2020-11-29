02630a829c72b827e38147af4b419131129a8521
flyingzambie <26560603+flyingzambie@users.noreply.github.com>
2020-11-29 13:57:30 -0500

    Some experimental changes to how the list is generated

--- src/react/src/components/discussion/discussionList.component.js
+++ src/react/src/components/discussion/discussionList.component.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Card } from 'react-bootstrap'
+import './discussionList.css'
 
 class DiscussionList extends Component {
     constructor(props) {
@@ -8,11 +10,51 @@ class DiscussionList extends Component {
         }
     }
     render() {
-        
+        var divStyle = {
+            margin: "1%",
+            border: "2px solid black",
+            borderRadius: "5px",
+            width: "75vw"
+          };
         return(
-                <div>
-                    
+            <div className = 'discussion-list-container'>
+                <div style = {{margin: "1%"}}>
+                    Discussion Thread: 
+                    <span>
+                        <button>Create Thread</button>
+                    </span>
                 </div>
+                <div className = 'discussion-list-list'>
+                    <button style = {divStyle}>
+                        <div style = {{marginLeft: "20px"}}>
+                            Title
+                        </div>
+                        <div>
+                            <span>Replies</span>
+                            <span>Date</span>
+                        </div>
+                    </button>
+                    <button style = {divStyle}>
+                        <div style = {{marginLeft: "20px"}}>
+                            Title
+                        </div>
+                        <div>
+                            <span>Replies</span>
+                            <span>Date</span>
+                        </div>
+                    </button>
+                    <button style = {divStyle}>
+                        <div style = {{marginLeft: "20px"}}>
+                            Title
+                        </div>
+                        <div>
+                            <span>Replies</span>
+                            <span>Date</span>
+                        </div>
+                    </button>
+                </div>
+                
+            </div>
             )
         }
 }
