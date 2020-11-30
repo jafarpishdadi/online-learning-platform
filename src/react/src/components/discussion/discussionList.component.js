9d5365b4cd030487c0a61a4e49fd5e343741bb52
flyingzambie <26560603+flyingzambie@users.noreply.github.com>
2020-11-30 17:11:37 -0500

    Systematic button generation

--- src/react/src/components/discussion/discussionList.component.js
+++ src/react/src/components/discussion/discussionList.component.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react'
 import { Card } from 'react-bootstrap'
 import './discussionList.css'
 
+let threads = [
+    {id: 1, title: "Title 1", replies: 0, date: "Soon" },
+    {id: 2, title: "Title 2", replies: 1, date: "Soon" },
+    {id: 3, title: "Title 3", replies: 2, date: "Soon" },   
+    ]
+
 class DiscussionList extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            
+            newThread : false
         }
     }
     render() {
@@ -19,39 +25,25 @@ class DiscussionList extends Component {
         return(
             <div className = 'discussion-list-container'>
                 <div style = {{margin: "1%"}}>
-                    Discussion Thread: 
+                    Discussion Threads: 
                     <span>
                         <button>Create Thread</button>
                     </span>
                 </div>
                 <div className = 'discussion-list-list'>
-                    <button style = {divStyle}>
-                        <div style = {{marginLeft: "20px"}}>
-                            Title
-                        </div>
-                        <div>
-                            <span>Replies</span>
-                            <span>Date</span>
-                        </div>
-                    </button>
-                    <button style = {divStyle}>
-                        <div style = {{marginLeft: "20px"}}>
-                            Title
-                        </div>
-                        <div>
-                            <span>Replies</span>
-                            <span>Date</span>
-                        </div>
-                    </button>
-                    <button style = {divStyle}>
-                        <div style = {{marginLeft: "20px"}}>
-                            Title
-                        </div>
-                        <div>
-                            <span>Replies</span>
-                            <span>Date</span>
-                        </div>
-                    </button>
+                    {threads.map(
+                        (thread) => 
+                            <button key = {thread.id} style = {divStyle}>
+                                <div style = {{marginLeft: "20px"}}>
+                                    {thread.title}
+                                </div>
+                                <div>
+                                    <span>{thread.replies}</span>
+                                    <span>{thread.date}</span>
+                                </div>
+                            </button>
+                    )}
+                    
                 </div>
                 
             </div>
