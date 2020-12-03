2688aa1edea3aad742591d434255d5d5feb62b41
flyingzambie <26560603+flyingzambie@users.noreply.github.com>
2020-12-03 18:20:32 -0500

    Everything except for redirect is done for discussionList

--- src/react/src/components/discussion/discussionList.component.js
+++ src/react/src/components/discussion/discussionList.component.js
@@ -4,28 +4,29 @@ import './discussionList.css'
 import CreateThread from './createThread.component.js'
 import axios from 'axios';
 
-let threads = [
-    {id: 1, title: "Title 1", replies: 0, date: "Soon" },
-    {id: 2, title: "Title 2", replies: 1, date: "Soon" },
-    {id: 3, title: "Title 3", replies: 2, date: "Soon" },
-    {id: 4, title: "Title 4", replies: 3, date: "Soon" },    
-    ]
-
-
 class DiscussionList extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            newThread : false
+            newThread : false,
+            threads: []
         }
     }
     componentDidMount() {
+        var resThread = {};
+        var thread = [];
         axios.get('http://127.0.0.1:8103/api/db_get_all_threads')
             .then(response => {
-
-                // console.log(response);
-                // console.log(response.data['email'])
-                // this.setState({email: response.data})
+                console.log(response.data);
+                for (var i = 0; i < response.data.length; i++){
+                    resThread['id'] = response.data[i]._id;
+                    resThread['title'] = response.data[i].title;
+                    resThread['replies'] = response.data[i].bodies.length - 1;
+                    resThread['date'] = response.data[i].timestamps[0];
+                    thread[i] = JSON.parse(JSON.stringify(resThread));
+                }
+                console.log(thread);
+                this.setState({threads: thread})
             })
 			.catch((error) => {
 			console.log(error)
@@ -56,15 +57,15 @@ class DiscussionList extends Component {
                 </div>
                 {this.state.newThread ? <CreateThread/> : null}
                 <div className = 'discussion-list-list'>
-                    {threads.map(
+                    {this.state.threads.map(
                         (thread) => 
                             <button key = {thread.id} style = {divStyle}>
                                 <div style = {{marginLeft: "20px"}}>
-                                    {thread.title}
+                                    Title: {thread.title}
                                 </div>
                                 <div>
-                                    <span>{thread.replies}</span>
-                                    <span>{thread.date}</span>
+                                    <span>Replies: {thread.replies}</span>
+                                    <span>Date: {thread.date}</span>
                                 </div>
                             </button>
                     )}
